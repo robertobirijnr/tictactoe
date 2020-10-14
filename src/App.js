542d8ac106f7affa377be88ad2e6d785a47aae5a
robertobirijnr <robertobirijnr@hotmail.com>
2020-10-14 10:29:13 +0000

    project starter

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Board from './components/Board'
+import './styles/root.scss'
+
+export default () => (
+  <div className="app">
+  <h1>Tic Tac Toe App</h1>
+    <Board/>
+  </div>
+);
